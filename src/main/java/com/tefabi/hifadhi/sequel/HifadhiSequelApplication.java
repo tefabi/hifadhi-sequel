14524999aeaa4f11f835c25a76d016c764ad04c6
Timothy Karani <kartimothy@gmail.com>
2022-12-04 01:20:41 +0300

    HIF-4 added axon dependecies

--- src/main/java/com/tefabi/hifadhi/sequel/HifadhiSequelApplication.java
+++ src/main/java/com/tefabi/hifadhi/sequel/HifadhiSequelApplication.java
@@ -2,8 +2,10 @@ package com.tefabi.hifadhi.sequel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class HifadhiSequelApplication {
 
 	public static void main(String[] args) {
