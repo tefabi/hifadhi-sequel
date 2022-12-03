c25936e361fd7d69350980dfe8608fd39550fc3f
Timothy Karani <kartimothy@gmail.com>
2022-12-04 00:58:57 +0300

    HIF-1 Created hifadhi sequel microservice

---
+++ src/main/java/com/tefabi/hifadhi/sequel/HifadhiSequelApplication.java
@@ -0,0 +1,13 @@
+package com.tefabi.hifadhi.sequel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HifadhiSequelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HifadhiSequelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tefabi/hifadhi/sequel/HifadhiSequelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tefabi.hifadhi.sequel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HifadhiSequelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
